c2f3ed6cac12248f080d37742b8d385823fc9edf
Rachel Greenwood <rachelgreenwood3301@gmail.com>
2023-09-06 12:18:07 -0500

    Fetches data from API with GET method

--- server/index.js
+++ server/index.js
@@ -6,4 +6,11 @@ const PORT = 3000;
 
 app.use(cors());
 
+app.get("/api", async (req, res) => {
+    const url = `https://opentdb.com/api.php?amount=10`;
+    const response = await fetch(url);
+    const quizData = await response.json()
+    res.json(quizData);
+})
+
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
