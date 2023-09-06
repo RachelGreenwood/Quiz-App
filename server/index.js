6b14fc7949b15178f5695159a3fe29638f5c8b66
Rachel Greenwood <rachelgreenwood3301@gmail.com>
2023-09-06 12:08:50 -0500

    Adds cors

--- server/index.js
+++ server/index.js
@@ -1,6 +1,9 @@
 import express from 'express';
+import cors from 'cors';
 
 const app = express();
 const PORT = 3000;
+
+app.use(cors());
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
